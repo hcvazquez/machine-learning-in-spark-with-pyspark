2e5563c5c07d531d039844827649f6eeeeb6366c
hcvazquez <hernan.c.vazquez@gmail.com>
2019-06-04 23:46:56 -0300

    refactoring

--- linear_regression/linear-regression.py
+++ linear_regression/linear-regression.py
@@ -1,34 +1,3 @@
-#import corr function from pyspark functions
-from pyspark.sql.functions import corr
-
-# check for correlation
-df.select([corr(x,'output') for x in df.columns]).show()
-
-#import vectorassembler to create dense vectors
-from pyspark.ml.feature import VectorAssembler
-
-#select the columns to create input vector
-df.columns
-
-#create the vector assembler 
-vec_assmebler=VectorAssembler(inputCols=['var_1', 'var_2', 'var_3', 'var_4', 'var_5'],outputCol='features')
-
-#transform the values
-features_df=vec_assmebler.transform(df)
-
-#validate the presence of dense vectors 
-features_df.printSchema()
-
-#view the details of dense vector
-features_df.select('features').show(5,False)
-
-#create data containing input features and output column
-model_df=features_df.select('features','output')
-model_df.show(5,False)
-
-#size of model df
-print((model_df.count(), len(model_df.columns)))
-
 # Split Data - Train & Test sets
 #split the data into 70/30 ratio for train test purpose
 train_df,test_df=model_df.randomSplit([0.7,0.3])
