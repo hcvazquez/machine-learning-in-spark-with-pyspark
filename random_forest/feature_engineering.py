dc5a9cfecb64dda2cf5dea0e1ea30372d03e3f50
hcvazquez <hernan.c.vazquez@gmail.com>
2019-06-05 00:04:55 -0300

    rename files after refactoring

--- random_forest/feature_engineering.py
+++ random_forest/feature_engineering.py
@@ -4,10 +4,3 @@ df_assembler = VectorAssembler(inputCols=['rate_marriage', 'age', 'yrs_married',
 df = df_assembler.transform(df)
 df.printSchema()
 df.select(['features','affairs']).show(10,False)
-
-#select data for building model
-model_df=df.select(['features','affairs'])
-train_df,test_df=model_df.randomSplit([0.75,0.25])
-train_df.count()
-train_df.groupBy('affairs').count().show()
-test_df.groupBy('affairs').count().show()
