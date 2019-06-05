a91e84c07fce70c275f04f49f1d1e3c18a682764
hcvazquez <hernan.c.vazquez@gmail.com>
2019-06-04 23:54:52 -0300

    refactoring

--- random_forest/random-forests.py
+++ random_forest/random-forests.py
@@ -1,23 +1,3 @@
-# Databricks notebook source
-import findspark
-findspark.init()
-
-#import SparkSession
-from pyspark.sql import SparkSession
-spark=SparkSession.builder.appName('random_forest').getOrCreate()
-
-#read the dataset
-df=spark.read.csv('affairs.csv',inferSchema=True,header=True)
-
-#check the shape of the data 
-print((df.count(),len(df.columns)))
-
-#printSchema
-df.printSchema()
-
-#view the dataset
-df.show(5)
-
 #Exploratory Data Analysis
 df.describe().select('summary','rate_marriage','age','yrs_married','children','religious').show()
 df.groupBy('affairs').count().show()
