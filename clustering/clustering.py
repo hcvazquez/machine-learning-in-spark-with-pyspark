a4c61717f2d0c9f3bf9b8404c04380c43176207f
hcvazquez <hernan.c.vazquez@gmail.com>
2019-06-04 23:41:27 -0300

    refactoring

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -1,14 +1,3 @@
-# Feature Engineering
-
-from pyspark.ml.feature import VectorAssembler
-
-input_cols=['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
-
-# Transform all features into a vector using VectorAssembler
-vec_assembler = VectorAssembler(inputCols = input_cols, outputCol='features')
-final_data = vec_assembler.transform(df)
-
-
 # Hyper parameter tuning
 
 errors=[]
