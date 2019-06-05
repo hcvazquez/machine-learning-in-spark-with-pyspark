83479109007b1e91c051a0ea4ab43d78f67607c3
hcvazquez <hernan.c.vazquez@gmail.com>
2019-06-04 23:44:58 -0300

    refactoring

--- linear_regression/linear-regression.py
+++ linear_regression/linear-regression.py
@@ -1,25 +1,3 @@
-# Create Spark Session
-from pyspark.sql import SparkSession
-spark=SparkSession.builder.appName('lin_reg').getOrCreate()
-
-#import Linear Regression from spark's MLlib
-from pyspark.ml.regression import LinearRegression
-
-#Load the dataset
-df=spark.read.csv('/FileStore/tables/Linear_regression_dataset.csv',inferSchema=True,header=True)
-
-#validate the size of data
-print((df.count(), len(df.columns)))
-
-#explore the data
-df.printSchema()
-
-#view statistical measures of data 
-df.describe().show(5,False)
-
-#sneak into the dataset
-df.head(3)
-
 #import corr function from pyspark functions
 from pyspark.sql.functions import corr
 
