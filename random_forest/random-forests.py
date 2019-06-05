fe46ec489b7e601d3472b2fff5bef18cfd5a3b5c
hcvazquez <hernan.c.vazquez@gmail.com>
2019-06-04 23:56:57 -0300

    refactoring

--- random_forest/random-forests.py
+++ random_forest/random-forests.py
@@ -1,17 +1,3 @@
-from pyspark.ml.feature import VectorAssembler
-
-df_assembler = VectorAssembler(inputCols=['rate_marriage', 'age', 'yrs_married', 'children', 'religious'], outputCol="features")
-df = df_assembler.transform(df)
-df.printSchema()
-df.select(['features','affairs']).show(10,False)
-
-#select data for building model
-model_df=df.select(['features','affairs'])
-train_df,test_df=model_df.randomSplit([0.75,0.25])
-train_df.count()
-train_df.groupBy('affairs').count().show()
-test_df.groupBy('affairs').count().show()
-
 from pyspark.ml.classification import RandomForestClassifier
 
 rf_classifier=RandomForestClassifier(labelCol='affairs',numTrees=50).fit(train_df)
