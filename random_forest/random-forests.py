df22a7415b6f8f91503e790d92be4b9b05817bad
hcvazquez <hernan.c.vazquez@gmail.com>
2019-06-04 23:55:46 -0300

    code refactoring

--- random_forest/random-forests.py
+++ random_forest/random-forests.py
@@ -1,13 +1,3 @@
-#Exploratory Data Analysis
-df.describe().select('summary','rate_marriage','age','yrs_married','children','religious').show()
-df.groupBy('affairs').count().show()
-df.groupBy('rate_marriage').count().show()
-df.groupBy('rate_marriage','affairs').count().orderBy('rate_marriage','affairs','count',ascending=True).show()
-df.groupBy('religious','affairs').count().orderBy('religious','affairs','count',ascending=True).show()
-df.groupBy('children','affairs').count().orderBy('children','affairs','count',ascending=True).show()
-df.groupBy('affairs').mean().show(5,True)
-
-
 from pyspark.ml.feature import VectorAssembler
 
 df_assembler = VectorAssembler(inputCols=['rate_marriage', 'age', 'yrs_married', 'children', 'religious'], outputCol="features")
